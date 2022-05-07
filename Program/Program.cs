1ff6059c2eec43cdcc7b0f90362e884724c95272
Mihail Mitev <95048979+mihailmitev@users.noreply.github.com>
2022-05-07 13:38:26 +0300

    Add files via upload

---
+++ Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Average
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, };
+
+            double average = Average.AverageArr(arr);
+
+            if (average == 3.5)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
